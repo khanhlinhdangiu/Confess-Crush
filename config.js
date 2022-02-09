cda4bf9959dd552206f1bd71095826972f3bda2f
khanhlinhdangiu <99307394+khanhlinhdangiu@users.noreply.github.com>
2022-02-09 15:10:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "iukhanhlinh",
+    introTitle: 'khanhlinhdangiu',
+    introDesc: `Càng ngày càng nhớ
+    Càng ngày càng nghiện
+    Càng ngày càng khó cai
+    Chắc là do yêu rùi`,
+    btnIntro: '^^Loveiu^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Tớ iu nhất ở cậu điểm gì<3',
+    btnReply: 'Chắc chắn khum <3',
+    reply: 'Tớ iu cậu<3 vầy thui',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Chúc cậu ngủ ngon nha tình iu của tuii.',
+    btnAccept: 'iu cậu nhìu lắm <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
